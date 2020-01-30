67e5cf60dfaf00553e6f5db3d8db95d1475fe4f7
Sergio Paz <alejandroax@live.com.mx>
2020-01-29 18:20:04 -0700

    After validation fields

--- Vidly/Vidly/Models/Movie.cs
+++ Vidly/Vidly/Models/Movie.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Vidly.Models;
 
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [NewMovieValidationAttribute]
         public string Name { get; set; }
        
         public DateTime? ReleaseDate { get; set; }
+
+        
         public DateTime? DateAdded { get; set; }
+
+       
         public int? Stock { get; set; }
 
         public Genre Genre { get; set; }
---
+++ Vidly/Vidly/Models/NewMovieValidationAttributes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class NewMovieValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var movie = (Movie)validationContext.ObjectInstance;
+
+           
+
+            if (movie.Name == null)
+            {
+                return new ValidationResult("plese add a name");
+            }
+           
+
+            return ValidationResult.Success;
+        }
+       
+
+
+    }
+}
